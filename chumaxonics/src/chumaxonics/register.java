3df86f2d2b840f10b052519bc9da55c8522fbd74
CHUMA GIFT <53385182+chumaxonics@users.noreply.github.com>
2021-05-28 18:28:18 +0200

    size changed to fit

--- chumaxonics/src/chumaxonics/register.java
+++ chumaxonics/src/chumaxonics/register.java
@@ -33,11 +33,11 @@ public class register extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 1026, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 465, Short.MAX_VALUE)
         );
 
         pack();
